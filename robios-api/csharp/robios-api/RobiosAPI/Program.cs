c6640f4cd1fdb0835cd7a0a6a099f25064339430
Leandro Dotta <leandrodotta@gmail.com>
2019-10-21 19:02:15 -0300

    Adding project template for C# (Visual Studio and Visual Studio Code)

---
+++ robios-api/csharp/robios-api/RobiosAPI/Program.cs
@@ -0,0 +1,20 @@
+﻿using ai.humanrobotics.api.csharp;
+using System;
+
+namespace ai.humanrobotics.examples
+{
+    class Program
+    {
+        private const string API_KEY = "9106b136-1424-4b2a-be0a-6e313dc2a80d";
+        private const string ROBOT_ADDRESS = "192.168.15.27";
+
+        static void Main(string[] args)
+        {
+            IRobios robios = RobiosApi.Get(API_KEY, ROBOT_ADDRESS);
+
+            robios.Say("Olá Mundo!");
+
+            robios.Close();
+        }
+    }
+}
