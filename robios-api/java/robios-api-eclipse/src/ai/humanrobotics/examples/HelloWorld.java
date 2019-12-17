86e643cc129e58c333cb5186af2fb58f16572363
Leandro Dotta <leandrodotta@gmail.com>
2019-12-17 10:15:39 -0300

    Update HelloWorld.java

--- robios-api/java/robios-api-eclipse/src/ai/humanrobotics/examples/HelloWorld.java
+++ robios-api/java/robios-api-eclipse/src/ai/humanrobotics/examples/HelloWorld.java
@@ -5,7 +5,7 @@ import io.humanrobotics.api.RobiosApi;
 
 public class HelloWorld {
 	private static final String API_KEY = "9106b136-1424-4b2a-be0a-6e313dc2a80d";
-	private static final String ROBOT_ADDRESS = "192.168.100.38";
+	private static final String ROBOT_ADDRESS = "127.0.0.1";
 	
 	public static void main(String[] args) throws Exception {
 		Robios robios = RobiosApi.get(API_KEY, ROBOT_ADDRESS);
